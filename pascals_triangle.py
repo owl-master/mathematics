7b9aee9315827d80e15eff9d028f9b1f09f8d214
valis1 <valis.source@gmail.com>
2018-03-17 07:06:32 +0200

    Add readme.md

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -2,7 +2,7 @@ import math
 
 class pascTriangle():
     def __init__(self,rows_qty):
-        if rows_qty>0 and self._is_int(rows_qty):
+        if rows_qty>0 and isinstance(rows_qty, int):
             self.rows=[[] for i in range(rows_qty)]
             for i in range(rows_qty):
                 j=0
@@ -10,8 +10,7 @@ class pascTriangle():
                     self.rows[i].append(self._binominal(i,j))
                     j+=1
         else:
-            print('Quantity of rows must be natural and >0')
-            raise ValueError
+            raise ValueError('Quantity of rows must be natural and >0')
     def get_binominal(self,n,k):
         return self.rows[n-1][k-1]
     def print_triangle(self):
@@ -19,14 +18,7 @@ class pascTriangle():
             print(','.join([str(int(x)) for x in i]))
     def _binominal(self,n,k):
         return math.factorial(n)/(math.factorial(k)*math.factorial(n-k))
-    def _is_int(self,x):
-        try:
-            int(x)
-            return True
-        except ValueError:
-            return False
 
-        
                     
             
         
